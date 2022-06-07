4958d2581603f04a4fa24a5a369151f607e52b61
RenatoConfuorto <renato.confuorto@outlook.it>
2022-06-07 17:38:49 +0200

    esercizio completato

--- index.php
+++ index.php
@@ -7,6 +7,36 @@
   <title>Document</title>
 </head>
 <body>
-  
+
+  <div>
+    <?php
+    //testo originale
+      $text = "
+        Il signore e la signora Dursley, di Privet Drive numero 4, erano orgogliosi di poter affermare che erano perfettamente normali, e grazie tante. Erano le ultime persone al mondo da cui aspettarsi che avessero a che fare con cose strane o misteriose, perché sciocchezze del genere proprio non le approvavano.
+      ";
+    ?>
+    <h3>Capitolo 1</h3>
+    <h2>Il bambino che è sopravvissuto (Lunghezza: <?php echo strlen($text); ?>)</h2>
+    <p>
+      <?php
+      echo $text;
+      ?>
+    </p>
+  </div>
+
+  <div>
+    <?php
+      //testo censurato
+      $word_to_remove = $_GET['word'];
+      $new_text = str_replace($word_to_remove, "***", $text);
+    ?>
+    <h3>Capitolo 1</h3>
+    <h2>Il bambino che è sopravvissuto (Lunghezza: <?php echo strlen($new_text); ?>)</h2>
+    <p>
+      <?php
+      echo $new_text;
+      ?>
+    </p>
+  </div>
 </body>
 </html>
